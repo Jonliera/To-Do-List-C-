ef771498ff26506fd5313855078cc57c251d3d83
Jonathan Liera <jonliera1@gmail.com>
2022-12-07 18:42:37 -0800

    add files to project

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace ToDoList.Models
@@ -6,14 +5,15 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
+    public int Id { get; }
     private static List<Item> _instances = new List<Item> {};
 
     public Item(string description)
     {
       Description = description;
       _instances.Add(this);
+      Id = _instances.Count;
     }
-
     public static List<Item> GetAll()
     {
       return _instances;
@@ -22,6 +22,10 @@ namespace ToDoList.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
